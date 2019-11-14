5da1d2a9f535d30315520726c04681b1ae211913
Attila Györffy <attila@attilagyorffy.com>
2019-11-14 14:55:48 +0100

    Move MultiSessionStrore under the ActionDispatch::Session namespace
    Bring in the functionality of multi_session_cache_store gem and strat writing tests for it

--- lib/multi_session_store.rb
+++ lib/multi_session_store.rb
@@ -1,6 +1,49 @@
 require "multi_session_store/version"
+require "action_dispatch"
 
-module MultiSessionStore
-  class Error < StandardError; end
-  # Your code goes here...
+module ActionDispatch
+  module Session
+    class MultiSessionStore < AbstractStore
+      def initialize(app, options = {})
+        @cache = options[:cache]
+        options[:expire_after] ||= @cache.options[:expires_in]
+        @param = options[:param]
+        @serializer = options[:serializer]
+
+        super
+      end
+
+      def find_session(env, sid)
+        sid ||= generate_sid
+        session = @serializer.parse(@cache.read(cache_key(env, sid)) || "{}")
+
+        [sid, session]
+      end
+
+      def write_session(env, sid, session, options)
+        key = cache_key(env, sid)
+
+        if session
+          @cache.write(key, @serializer.dump(session), expires_in: options[:expire_after])
+        else
+          @cache.delete(key)
+        end
+
+        sid
+      end
+
+      def delete_session(env, sid, options)
+        @cache.delete(cache_key(env, sid))
+
+        sid
+      end
+
+    private
+
+      def cache_key(env, sid)
+        subsession_id = env.params[@param] || "no_subsession"
+        "_session_id:#{sid}:#{subsession_id}"
+      end
+    end
+  end
 end
--- spec/multi_session_store_spec.rb
+++ spec/multi_session_store_spec.rb
@@ -1,4 +1,56 @@
-RSpec.describe MultiSessionStore do
+RSpec.describe ActionDispatch::Session::MultiSessionStore do
+  describe '#initialize' do
+    context 'with a :cache option' do
+      subject do
+        ActionDispatch::Session::MultiSessionStore.new(:app, { cache: cache } )
+      end
+
+      let(:cache) do
+        OpenStruct.new(options: {})
+      end
+
+      it 'takes that as a passed in parameter' do
+        expect(subject.default_options[:cache]).to eql(cache)
+      end
+    end
+
+    context 'with an :expire_after option' do
+      subject(:multisession) { ActionDispatch::Session::MultiSessionStore.new(:app, { expire_after: 1 }) }
+
+      it 'persists it in default_options' do
+        expect(multisession.default_options[:expire_after]).to eq 1
+      end
+    end
+
+    context 'without an :expire_after option' do
+      subject(:multisession) { ActionDispatch::Session::MultiSessionStore.new(:app, { cache: cache }) }
+
+      let(:cache) do
+        OpenStruct.new(options: { expires_in: 123 })
+      end
+
+      it 'sets its value to @cache.options[:expires_in]' do
+        expect(multisession.default_options[:expire_after]).to eql(123)
+      end
+    end
+
+    context 'with a param option' do
+      subject(:multisession) { ActionDispatch::Session::MultiSessionStore.new(:app, { expire_after: 1, param: 'my_store_param' }) }
+
+      it 'persists it in default_options' do
+        expect(multisession.default_options[:param]).to eql('my_store_param')
+      end
+    end
+
+    context 'with a serializer option' do
+      subject(:multisession) { ActionDispatch::Session::MultiSessionStore.new(:app, { expire_after: 1, serializer: 'JSON' }) }
+
+      it 'persists it in default_options' do
+        expect(multisession.default_options[:serializer]).to eql('JSON')
+      end
+    end
+  end
+
   it "has a version number" do
     expect(MultiSessionStore::VERSION).not_to be nil
   end
