ed76a826c80c526d12b08831cba9c46854a5d6c6
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2019-11-18 12:17:36 +0100

    AA-2269: Make sure Subsession generator middleware is place before session store middleware so that subsession ID is available when session is stored

--- lib/multi_session_store/railtie.rb
+++ lib/multi_session_store/railtie.rb
@@ -3,7 +3,9 @@ module MultiSessionStore
     config.multi_session_store = ActiveSupport::OrderedOptions.new
 
     initializer 'multi_session_store.add_middleware' do |app|
-      app.config.middleware.use MultiSessionStore::SubsessionGeneratorMiddleware, exclude_paths: app.config.multi_session_store.exclude_paths
+      app.config.middleware.insert_before ActionDispatch::Session::MultiSessionStore,
+                                          MultiSessionStore::SubsessionGeneratorMiddleware,
+                                          exclude_paths: app.config.multi_session_store.exclude_paths
     end
 
     config.after_initialize do
